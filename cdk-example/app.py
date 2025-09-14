1127d6fbac139ea355f4f4138c9c85115263de0b
Ritvik Nag <nagrim7@gmail.com>
2025-09-13 23:36:12 -0400

    Fix tests

---
+++ cdk-example/app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import aws_cdk as cdk
+
+from cdk_example.cdk_example_stack import CdkExampleStack
+
+
+app = cdk.App()
+CdkExampleStack(app, "CdkExampleStack")
+
+app.synth()
